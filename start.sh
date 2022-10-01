ebf30cb28f0ffd86995fd38b3babf7f2119b21d0
patrick11514 <ja@patrick115.eu>
2022-10-02 00:10:28 +0200

    2

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@ rm -rf /home/container/tmp/*
 
 echo "Starting Nginx..."
 echo "Successfully started"
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf -p /home/container/
+/usr/sbin/nginx -c /home/webhost/nginx/nginx.conf -p /home/container/
